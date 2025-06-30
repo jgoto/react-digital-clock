b13449bdf3b07a9ca188233760c26339fbaeaa8d
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-06-30 10:31:04 -0400

    refactored time formatting into a function that can be easier updated in preperation for 12/24 hour mode with am/pm

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,10 +4,15 @@ import styles from "./Display.module.css";
 
 function Display(){
     const [date, setTime] = useState(new Date())
-    const hours = String(date.getHours()).padStart(2,0);
-    const minutes = String(date.getMinutes()).padStart(2,0);
-    const seconds = String(date.getSeconds()).padStart(2,0);
-    const digitValues = (hours+":"+minutes+":"+seconds).split("");
+
+    function formatTime(date, is24Hours = true){
+        const hours = String(date.getHours()).padStart(2,0);
+        const minutes = String(date.getMinutes()).padStart(2,0);
+        const seconds = String(date.getSeconds()).padStart(2,0);
+        return (hours+":"+minutes+":"+seconds).split("");
+    }
+    
+    const digitValues = formatTime(date);
 
     useEffect(()=>{
         const intervalId = setInterval(()=>{
